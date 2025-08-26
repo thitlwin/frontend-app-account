1fe9a8f0f281cd853106e2314ce7bdc8f980a85f
thit <saithitlwin@gmail.com>
2025-08-26 16:52:23 +0700

    add myanmar language

--- src/account-settings/site-language/constants.js
+++ src/account-settings/site-language/constants.js
@@ -5,6 +5,11 @@ const siteLanguageList = [
     released: true,
   },
   {
+    code: 'my-MM',
+    name: 'Myanmar (Burmese)',
+    released: true,
+  },
+  {
     code: 'ar',
     name: 'العربية',
     released: true,
