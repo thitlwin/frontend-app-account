8e278fd1e5a5fc85652c6624db21a9eb750bba1d
thit <saithitlwin@gmail.com>
2025-08-27 00:29:37 +0700

    - added Myanmar language for mfe-account

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,7 +1,7 @@
-import mmMessages from './messages/my-MM.json';
+import mmMessages from './messages/my.json';
 
 const messages = {
-  'my-MM': mmMessages,
-}
+  'my': mmMessages, // Language code (my) should match the code in constants.js. And the language code should match exactly with standard language codes. For myanmar, there is not region code.
+};
 
-export default [];
+export default [messages];
